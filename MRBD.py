ac28028dc4f5278cebb752900ffb2c79b201675a
MORSHED-404 <141574484+MORSHED-404@users.noreply.github.com>
2023-08-20 20:53:21 +0600

    Update MRBD.py

--- MRBD.py
+++ MRBD.py
@@ -1,2 +1,31 @@
-from MRBD import MR_morshed
-MR_morshed()
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from MRBD64 import menu
+ 
+        menu()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from MRBD32 import menu
+ 
+ 
+        menu()
+ 
