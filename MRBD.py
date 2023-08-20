e86040ec08d1ed4224498d4e4caad0a243c77844
MORSHED-404 <141574484+MORSHED-404@users.noreply.github.com>
2023-08-20 18:12:51 +0600

    Update MRBD.py

--- MRBD.py
+++ MRBD.py
@@ -1,3 +1,16 @@
-from MRBD64 import main
+import os, sys
+os.system('clear')
+os.system('git pull')
+try:
+    __import__("MRBD64").file()
+except Exception as e:
+    exit(str(e))
 
-from MRBD32 import main
+
+import os, sys
+os.system('clear')
+os.system('git pull')
+try:
+    __import__("MRBD32").file()
+except Exception as e:
+    exit(str(e))
